90e241fd16ff6cc1e78720cf111dbfe86ddd89f2
NikhilKudva <f20212409@goa.bits-pilani.ac.in>
2024-12-23 00:10:23 +0530

    git check

---
+++ index.js
@@ -0,0 +1,73 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+let data = [];
+let nextID = 1;
+
+//post is used when we want to save the data in the database, can use get as well
+
+//adding a new data
+app.post("/data", (req, res) => {
+  const { name, price } = req.body;
+  const newItem = { id: nextID++, name, price };
+  data.push(newItem);
+  res.status(201).send(newItem);
+});
+
+//get all data
+app.get("/data", (req, res) => {
+  res.status(201).send(data);
+});
+
+//update
+app.put("/data/:id", (req, res) => {
+  const d = data.find((t) => t.id === parseInt(req.params.id));
+  if (!d) {
+    return res.status(404).send("data not found");
+  } else {
+    const { name, price } = req.body;
+    d.name = name;
+    d.price = price;
+    res.send(200).send(d);
+  }
+});
+
+//delete data
+app.delete("/data/:id", (req, res) => {
+  const index = data.findIndex((t) => t.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("data not found");
+  } else {
+    data.splice(index, 1);
+    return res.status(200).send("delete successful");
+  }
+});
+
+//retreiving data
+app.get("/data/:id", (req, res) => {
+  const d = data.find((t) => t.id === parseInt(req.params.id));
+  if (!d) {
+    return res.status(404).send("data not found");
+  } else {
+    return res.status(202).send(d);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello there");
+});
+
+app.get("/login", (req, res) => {
+  res.send("login checking");
+});
+
+app.get("/check", (req, res) => {
+  res.send("checking the check route");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at port : ${port} ... `);
+});
